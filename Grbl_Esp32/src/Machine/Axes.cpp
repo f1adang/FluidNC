f1adang/FluidNC

commit 62eea5d397bfbccc29938e4b34041728dca39f22

Axes skipped the validate phase

Grbl_Esp32/src/Machine/Axes.cpp
@@ -208,7 +208,8 @@ namespace Machine {
 
             if (handler.handlerType() == Configuration::HandlerType::Runtime || handler.handlerType() == Configuration::HandlerType::Parser ||
                 handler.handlerType() == Configuration::HandlerType::AfterParse ||
-                handler.handlerType() == Configuration::HandlerType::Generator) {
+                handler.handlerType() == Configuration::HandlerType::AfterParse ||
+                handler.handlerType() == Configuration::HandlerType::Validator) {
                 handler.section(tmp, _axis[a]);
             }
         }

commit 69618bc96fbed841c8d1196a429203042a598537

Fixed $cd for axes
A typo in 62eea5d39 broke it

Grbl_Esp32/src/Machine/Axes.cpp
@@ -209,6 +209,7 @@ namespace Machine {
             if (handler.handlerType() == Configuration::HandlerType::Runtime || handler.handlerType() == Configuration::HandlerType::Parser ||
                 handler.handlerType() == Configuration::HandlerType::AfterParse ||
                 handler.handlerType() == Configuration::HandlerType::AfterParse ||
+                handler.handlerType() == Configuration::HandlerType::Generator ||
                 handler.handlerType() == Configuration::HandlerType::Validator) {
                 handler.section(tmp, _axis[a]);
             }